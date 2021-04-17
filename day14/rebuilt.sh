225396b7edeb2aa2779d5c4c7a1e7f42d1cea25d
Phornhathai <74613199+Phornhathai@users.noreply.github.com>
2021-04-17 19:08:41 +0700

    update dockerfile

---
+++ day14/rebuilt.sh
@@ -0,0 +1,9 @@
+sudo docker build -t node-day13-img:latest . 
+
+sudo docker run --name node-day13 \
+        --network=network1 \
+        --restart=always \
+        --link mongo:db \
+        -d node-day13-img:latest
+
+        
