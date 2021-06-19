3c5191580a1de8849bb80018f8f84eae9138da35
Phornhathai <74613199+Phornhathai@users.noreply.github.com>
2021-06-19 20:23:38 +0700

    update app.js

---
+++ day15/lib/ServStatic.js
@@ -0,0 +1,28 @@
+module.exports = {
+    serv
+}
+const http = require('http')
+const fs = require('fs')
+// --------------------------------------------------
+const PORT = process.env.PORT || 9761
+// --------------------------------------------------
+
+let server = http.createServer( onListenner )
+server.listen(PORT)
+console.log('Server Start at ' + PORT)
+
+// --------------------------------------------------
+
+function serv(request,response){
+    let fullpath
+    // console.log( process.cwd() + request.url )
+     console.log(request.url)
+     if(request.url === '/'){
+         fullpath = process.cwd() + '/static/index.htm'
+     }else{
+         fullpath = process.cwd() + request.url
+     }
+         
+    
+
+}
